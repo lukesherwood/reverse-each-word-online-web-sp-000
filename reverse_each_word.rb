e3ee19f0188ee115280a5331d909e84c47411410
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-30 17:14:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   array = []
   array = string.split( )
-  array.collect{|words| words.reverse}
+  arrayrevarray.collect{|words| words.reverse}
   array.join(" ")
 end
